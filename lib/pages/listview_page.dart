ec8695fdefb903906524bb58881d2ab9573e8e41
AyrtonAldayrNevesEstrada <ayrton.24.6.95@gmail.com>
2020-01-16 19:53:17 -0500

    se creo el RefrehsIndicator, el cual realiza el efecto de refrescar

--- lib/pages/listview_page.dart
+++ lib/pages/listview_page.dart
@@ -47,15 +47,29 @@ class _ListaPageState extends State<ListaPage> {
   }
 
   Widget _crearListView() {
-    return ListView.builder(
-        controller: _scrollController,
-        itemCount: _listaNumeros.length,
-        itemBuilder: (BuildContext context, int index) {
-          final imagen = _listaNumeros[index];
-          return FadeInImage(
-              placeholder: AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage('https://picsum.photos/id/$imagen/500/300'));
-        });
+    return RefreshIndicator(
+      onRefresh: obtenerPagina1,
+      child: ListView.builder(
+          controller: _scrollController,
+          itemCount: _listaNumeros.length,
+          itemBuilder: (BuildContext context, int index) {
+            final imagen = _listaNumeros[index];
+            return FadeInImage(
+                placeholder: AssetImage('assets/jar-loading.gif'),
+                image:
+                    NetworkImage('https://picsum.photos/id/$imagen/500/300'));
+          }),
+    );
+  }
+
+  Future<Null> obtenerPagina1() async{
+    final duration  = new Duration(seconds: 2);
+    new Timer(duration, (){
+      _listaNumeros.clear();
+      _ultimoItem++;
+      _agregar10();
+    });
+    return Future.delayed(duration);
   }
 
   void _agregar10() {
